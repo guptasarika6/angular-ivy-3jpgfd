4ae0ba93a8cc11aa7371c3515fb97f08e5b469ed
guptasarika6 <guptasarika6@gmail.com>
2022-01-16 18:48:27 +0530

    4 files modified, 1 added, 2 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component, VERSION } from '@angular/core';
+import * as employeeData from './assets/employee.json';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
+
+interface Employee {
+  id: Number;
+  first_name: String;
+  last_name: String;
+  email: String;
+  gender: String;
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  employeeForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.employeeForm = this.fb.group({
+      id: '',
+      first_name: '',
+      last_name: '',
+      gender: '',
+      email: '',
+    });
+  }
+
+  employees: Employee[] = employeeData;
+
+  onSubmit() {
+    employeeData.concat(this.employeeForm.value);
+  }
 }
